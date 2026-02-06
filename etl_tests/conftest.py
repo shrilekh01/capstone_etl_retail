d0c720ee141a809d607e5c468f44fae5ff3b2160
shrilekh01 <shrilekhshrikhande06@gmail.com>
2026-02-06 20:14:16 +0530

    Skip DB tests in CI

--- etl_tests/conftest.py
+++ etl_tests/conftest.py
@@ -1,14 +1,32 @@
+import os
 import pytest
 import pandas as pd
 from sqlalchemy import create_engine
 
-# 🔧 Database connections
+# Detect CI environment
+IS_CI = os.getenv("CI") == "true"
+
 SOURCE_DB_URL = "mysql+pymysql://root:sunbeam@localhost/retail_src"
 DWH_DB_URL = "mysql+pymysql://root:sunbeam@localhost/retaildwh"
 
 
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if "etl_tests" in item.nodeid:
+            if any(layer in item.nodeid for layer in [
+                "layer1_",
+                "layer2_",
+                "layer3_",
+                "layer4_",
+                "layer5_"
+            ]):
+                item.add_marker(pytest.mark.db)
+                
 @pytest.fixture(scope="session")
 def source_engine():
+    if IS_CI:
+        pytest.skip("Skipping DB connection in CI")
+
     engine = create_engine(SOURCE_DB_URL)
     yield engine
     engine.dispose()
@@ -16,6 +34,9 @@ def source_engine():
 
 @pytest.fixture(scope="session")
 def dwh_engine():
+    if IS_CI:
+        pytest.skip("Skipping DB connection in CI")
+
     engine = create_engine(DWH_DB_URL)
     yield engine
     engine.dispose()
